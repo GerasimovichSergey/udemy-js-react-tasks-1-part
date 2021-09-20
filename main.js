e949c3f340e8d28724e86c005c309c6410b8611d
Gerasimovich Sergey <wmmoee@gmail.com>
2021-09-21 00:56:15 +0300

    feat: task 3

--- main.js
+++ main.js
@@ -65,10 +65,8 @@ showMyDB(personalMovieDB.private);
 
 function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
-        const genre = prompt(`Ваш любимый жанр под номером ${i}`, '');
-        personalMovieDB.genres[i - 1] = genre;
+        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`, '');
     }
-    console.log(personalMovieDB);
 }
 
 writeYourGenres();
